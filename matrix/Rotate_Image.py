5e5175ab197efaf251317e0fc17ad008588cbec0
shawn <shawnang167@gmail.com>
2022-06-01 11:05:55 +0800

    chores: revise on matrix

--- matrix/Rotate_Image.py
+++ matrix/Rotate_Image.py
@@ -1,16 +1,33 @@
 """
-date: 2/3/22
-question: 48
-topic: matrix, array, math
+No: 48
+Topic: matrix, array, math
 
-strategy:
-1. the reverse the matrix
-2. transpose the matrix (transpose = row become column and column become row)
+##############################################
+Question: 
+You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+Example:
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[7,4,1],[8,5,2],[9,6,3]]
+
+Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+
+Constraints:
+n == matrix.length == matrix[i].length
+1 <= n <= 20
+-1000 <= matrix[i][j] <= 1000
+##############################################
+
+Approach:
+1. Reverse the matrix, meaning making the first row the last row, and so on
+2. Transpose the matrix (transpose = row become column and column become row), how?, matrix[row][col] = matrix[col][row]
 """
 
 
 class Solution:
-    def rotate(self, matrix: List[List[int]]) -> None:
+    def rotate(self, matrix) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
@@ -27,3 +44,9 @@ class Solution:
         for i in range(len(matrix)):
             for j in range(i):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        print(matrix)
+
+
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+solution = Solution().rotate(matrix)
+# print(matrix[0][1], matrix[1][0])
