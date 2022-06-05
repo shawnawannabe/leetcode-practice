99ef165c0ec7f16bd63c4a282444a219b41b62b7
shawn <shawnang167@gmail.com>
2022-06-05 12:33:18 +0800

    chores: revise on spiral matrix

--- matrix/Spiral_Matrix.py
+++ matrix/Spiral_Matrix.py
@@ -1,51 +1,70 @@
 """
-date: 26/3/22
-question: 54
-topic: matrix
+No: 54
+Topic: Matrix, Array, Simulation
 
-approach:
-1. set and name the variables correctly and thats about it
-2. watch out for edge cases tho
+#################################################
+Given an m x n matrix, return all elements of the matrix in spiral order.
+
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [1,2,3,6,9,8,7,4,5]
+
+Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+
+Constraints:
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 10
+-100 <= matrix[i][j] <= 100
+#################################################
+
+Approach:
+1. Set and name the variables correctly and thats about it
+2. Watch out for edge cases tho
 """
 
 
-def spiralOrder(matrix):
-    height = len(matrix)
-    width = len(matrix[0])
+class Solution:
 
-    top = 0
-    bottom = height - 1
-    left = 0
-    right = width - 1
+    def spiralOrder(init, matrix):
+        height = len(matrix)
+        width = len(matrix[0])
 
-    ans = []
-    while top < bottom and left < right:
-        for col in range(left, right):
-            ans.append(matrix[top][col])
+        top = 0
+        bottom = height - 1
+        left = 0
+        right = width - 1
 
-        for row in range(top, bottom):
-            ans.append(matrix[row][right])
+        ans = []
+        while top < bottom and left < right:
+            for col in range(left, right):
+                ans.append(matrix[top][col])
 
-        for col in range(right, left, -1):
-            ans.append(matrix[bottom][col])
+            for row in range(top, bottom):
+                ans.append(matrix[row][right])
 
-        for row in range(bottom, top, -1):
-            ans.append(matrix[row][left])
+            for col in range(right, left, -1):
+                ans.append(matrix[bottom][col])
 
-        top += 1
-        bottom -= 1
-        left += 1
-        right -= 1
+            for row in range(bottom, top, -1):
+                ans.append(matrix[row][left])
 
-    # If a matrix remain inside it is either a 1xn or a mx1
-    # a linear scan will return the same order as spiral for these
-    if len(ans) < height*width:
-        for row in range(top, bottom+1):
-            for col in range(left, right+1):
-                ans.append(matrix[row][col])
+            top += 1
+            bottom -= 1
+            left += 1
+            right -= 1
 
-    return ans
+        # If a matrix is either a 1xn or a mx1 | eg. [[1, 2, 3]] or [[1], [2], [3]]
+        # this also applies to matrix remaining | eg. [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+        # a linear scan will return the same order as spiral for these
+        if len(ans) < height * width:
+            for row in range(top, bottom+1):
+                for col in range(left, right+1):
+                    ans.append(matrix[row][col])
+
+        print(ans)
+        return ans
 
 
-matrix = [[1], [2], [3]]
-spiralOrder(matrix)
+matrix = [[1, 2, 3]]
+solution = Solution().spiralOrder(matrix)
