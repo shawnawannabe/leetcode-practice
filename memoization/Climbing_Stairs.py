f6745be5d3719d269a8ad8a143faebd4b23939a9
shawn <shawnang167@gmail.com>
2022-06-08 22:12:24 +0800

    revise on climbing_stairs

--- memoization/Climbing_Stairs.py
+++ memoization/Climbing_Stairs.py
@@ -1,23 +1,34 @@
 """
-date: 15/4/2022
-no.: 62
-topic: dynamic programming
+No.: 62
+Topic: Dynamic programming, Math, Memoization
 
-question:
+#################################################################
+Question:
 You are climbing a staircase. It takes n steps to reach the top.
 Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top? 
 
-example:
+Example:
 Input: n = 2
 Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
 
 Input: n = 3
 Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
 
-strategy:
-1. it is actually a pattern, the solution = n-1 + n-2
-2. why? n = 3, output = 3, | n = 4 , output = 5
-3. the python syntax: a, b = b , a+b  means exactly this 
+Constraints:
+1 <= n <= 45
+#################################################################
+
+Approach:
+1. It is actually a pattern, the solution = n-1 + n-2
+2. Why? n = 3, output = 3, | n = 4 , output = 5
+3. The python syntax: a, b = b , a+b  means exactly this 
 """
 
 
@@ -33,9 +44,9 @@ class Solution:
             # a = b
             # b = tmp
             a, b = b, a + b  # same meaning as the commented code above
+        print(a)
         return a
 
 
 n = 2
 a = Solution.climbStairs(n)
-print(a)
